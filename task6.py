c86d6c9d45d0b907f82b9fc8342f191556dade69
qqreyn <duch.jan.8@gmail.com>
2026-02-25 16:30:19 +0100

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,11 @@
+foods = {
+    "fruits": {
+        "Red": "Apple",
+        "Yellow": "Banana",
+        "Blue": "Blueberry"
+    },
+    "Fast food": "Pizza",
+    "Vegetable": "Carrot"
+}
+
+print(foods)
